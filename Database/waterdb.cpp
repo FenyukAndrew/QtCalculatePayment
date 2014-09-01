deae18343338bc598ae076c9ae28dc143ea98bb6
Fenyuk Andrew <fenyukandrew@gmail.com>
2014-09-01 12:15:49 +0400

    Исправлена формула подсчета в Database/waterdb.cpp

--- Database/waterdb.cpp
+++ Database/waterdb.cpp
@@ -15,7 +15,8 @@ WaterDB::~WaterDB()
 //SELECT * FROM Water order by _id DESC limit 3 - При расчете
 //TOP = DESC LIMIT 1
 
-bool WaterDB::get_last_record(Water_record* m_water_record)
+//НЕВЕРНО - вычисления производяться за преведущий месяц
+/*bool WaterDB::get_last_record(Water_record* m_water_record)
 {
     //if (!m_water_record) return;
     //Лучше сортировать не по значению, а по _id = иначе неправильный ввод значения может все испортить
@@ -66,6 +67,58 @@ bool WaterDB::get_last_record(Water_record* m_water_record)
                 //и они были актуальны на время оплаты
                 m_water_record->Tariff=cur_tariff;
                 m_water_record->Sum=m_water_record->Tariff*(value_last_month-value_two_month_ago);
+                m_water_record->Sum_Commission=(m_water_record->Sum)*c_percent_Sberbank;//(1+0.01);
+            }
+        }
+        return true;
+    }
+    return false;
+}*/
+
+//Исправленная версия - теперь сумма вычисляется за текущий месяц, а не за прошедший
+bool WaterDB::get_last_record(Water_record* m_water_record)
+{
+    //if (!m_water_record) return;
+    //Лучше сортировать не по значению, а по _id = иначе неправильный ввод значения может все испортить
+    //Записи идут в обратном порядке - вначале самая последняя, затем предпоследняя
+    if (!a_query->exec("SELECT Date_Input_Value,Value,Sum,Sum_Commission,Date_Payment,Tariff,Month_Payment,Year_Payment FROM Water order by _id DESC limit 2"))//Value
+    {
+        qDebug() << "Error select Water";
+        return false;
+    }
+    QSqlRecord rec = a_query->record();
+
+    if (a_query->next())
+    {
+        //вычисления производяться на основе 2 преведущих месяцев!!!
+        m_water_record->Init();//Все значения возвращаются в исходные
+        m_water_record->Date_Input_Value=a_query->value(rec.indexOf("Date_Input_Value")).toDateTime();
+        m_water_record->Value=a_query->value(rec.indexOf("Value")).toInt();
+        m_water_record->Sum=a_query->value(rec.indexOf("Sum")).toDouble();
+        m_water_record->Sum_Commission=a_query->value(rec.indexOf("Sum_Commission")).toDouble();
+        m_water_record->Date_Payment=a_query->value(rec.indexOf("Date_Payment")).toDateTime();
+        m_water_record->Tariff=a_query->value(rec.indexOf("Tariff")).toDouble();
+        int month=a_query->value(rec.indexOf("Month_Payment")).toInt();
+        int year=a_query->value(rec.indexOf("Year_Payment")).toInt();
+        m_water_record->Month_Year_Payment=QDate(year,month,1);
+
+        //qDebug() << "Date_Input_Value= " << a_query->value(rec.indexOf("Date_Input_Value")).toString();
+        //qDebug() << "Date_Payment= " << a_query->value(rec.indexOf("Date_Payment")).toDateTime();
+
+        int value_last_month=0;
+        if (a_query->next())
+        {
+            //Получение значения за преведущий месяц
+            value_last_month=a_query->value(rec.indexOf("Value")).toInt();
+
+            //Вычисления суммы производяться когда есть преведущее значение
+            if (!m_water_record->Date_Payment.isValid())
+            {
+                //Если оплата не произведена - производяться вычисления
+                //Чтоб их было видно перед добавлением в базу
+                //и они были актуальны на время оплаты
+                m_water_record->Tariff=cur_tariff;
+                m_water_record->Sum=m_water_record->Tariff*(m_water_record->Value-value_last_month);
                 m_water_record->Sum_Commission=(m_water_record->Sum)*c_percent_Sberbank;//(1+0.01);
             }
         }
